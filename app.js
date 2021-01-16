2a08bedf4aa7ea78c83ccc87fd6b63f476c2780a
DevAman-sudo <devaman.net@gmail.com>
2021-01-16 19:46:09 +0545

    typescript <void / null / undefined>

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ var myString = "Hello World";
 var myNumber = 1;
 var myBoolean = true;
 var Any = "Anything @ " + 123; // can be anithing string number or boolean //
+console.log(myString, myNumber, myBoolean, Any);
 var stringArr = ['hello', 'world'];
 var numberArr = [1, 2, 3];
 var booleanArr = [true, false];
 var anyArr = ['hello', 1, true];
+console.log(stringArr, numberArr, booleanArr, anyArr);
 var stringArr1 = ['hello', 'world'];
 var numberArr1 = [1, 2, 3];
 var booleanArr1 = [true, false];
 var anyArr1 = ['hello', 1, true];
-console.log(myString, myNumber, myBoolean, Any);
-console.log(stringArr, numberArr, booleanArr, anyArr);
 console.log(stringArr1, numberArr1, booleanArr1, anyArr1);
+// Tuples //
+var StringNumberTuple = ['hello', 1];
+console.log(StringNumberTuple);
+// Void || null / undefined //
+var myVoid = null; // can be both null and undefined //
+var nullVoid = null;
+var undefinedVoid = undefined;
+console.log(myVoid, nullVoid, undefinedVoid);
--- app.ts
+++ app.ts
@@ -2,17 +2,26 @@ let myString: string = "Hello World";
 let myNumber: number = 1;
 let myBoolean: boolean = true;
 let Any: any = `Anything @ ${123}`; // can be anithing string number or boolean //
+console.log(myString , myNumber , myBoolean , Any);
 
 let stringArr: string[] = ['hello' , 'world'];
 let numberArr: number[] = [1 , 2 , 3];
 let booleanArr: boolean[] = [true , false];
 let anyArr: any[] = ['hello' , 1 , true];
+console.log(stringArr , numberArr , booleanArr , anyArr);
 
 let stringArr1: Array<string> = ['hello' , 'world'];
 let numberArr1: Array<number> = [1 , 2 , 3];
 let booleanArr1: Array<boolean> = [true , false];
 let anyArr1: Array<any> = ['hello' , 1 , true];
-
-console.log(myString , myNumber , myBoolean , Any);
-console.log(stringArr , numberArr , booleanArr , anyArr);
 console.log(stringArr1 , numberArr1 , booleanArr1 , anyArr1);
+
+// Tuples //
+let StringNumberTuple: [string , number] = ['hello' , 1];
+console.log(StringNumberTuple);
+
+// Void || null / undefined //
+let myVoid: void = null; // can be both null and undefined //
+let nullVoid: null = null;
+let undefinedVoid: undefined = undefined;
+console.log(myVoid , nullVoid , undefinedVoid);

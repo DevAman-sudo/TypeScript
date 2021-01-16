f19df7d7bf0edeb14375765b08cd1cd8b4323b6c
DevAman-sudo <devaman.net@gmail.com>
2021-01-16 19:28:51 +0545

    typescript Array update

--- app.ts
+++ app.ts
@@ -8,5 +8,11 @@ let numberArr: number[] = [1 , 2 , 3];
 let booleanArr: boolean[] = [true , false];
 let anyArr: any[] = ['hello' , 1 , true];
 
+let stringArr1: Array<string> = ['hello' , 'world'];
+let numberArr1: Array<number> = [1 , 2 , 3];
+let booleanArr1: Array<boolean> = [true , false];
+let anyArr1: Array<any> = ['hello' , 1 , true];
+
 console.log(myString , myNumber , myBoolean , Any);
 console.log(stringArr , numberArr , booleanArr , anyArr);
+console.log(stringArr1 , numberArr1 , booleanArr1 , anyArr1);

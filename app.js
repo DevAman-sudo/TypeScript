4340cb3f6f9b60ce7c557d7d1a7ee0552d0cbad3
DevAman-sudo <devaman.net@gmail.com>
2021-01-16 19:13:28 +0545

    working with typescript

---
+++ app.js
@@ -0,0 +1,5 @@
+var myString = "Hello World";
+var myNumber = 1;
+var myBoolean = true;
+var Any = "Anything @ " + 123; // can be anithing string number or boolean // 
+console.log(myString, myNumber, myBoolean, Any);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let myString: string = "Hello World";
+let myNumber: number = 1;
+let myBoolean: boolean = true;
+let Any: any = `Anything @ ${123}`; // can be anithing string number or boolean // 
+
+console.log(myString , myNumber , myBoolean , Any);

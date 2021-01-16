f864e3037d1ff9dc2e43be3772bfbe54c692f456
DevAman-sudo <devaman.net@gmail.com>
2021-01-16 19:21:38 +0545

    typescript syntax object array

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var myString = "Hello World";
 var myNumber = 1;
 var myBoolean = true;
-var Any = "Anything @ " + 123; // can be anithing string number or boolean // 
+var Any = "Anything @ " + 123; // can be anithing string number or boolean //
+var stringArr = ['hello', 'world'];
+var numberArr = [1, 2, 3];
+var booleanArr = [true, false];
+var anyArr = ['hello', 1, true];
 console.log(myString, myNumber, myBoolean, Any);
+console.log(stringArr, numberArr, booleanArr, anyArr);
--- app.ts
+++ app.ts
@@ -1,6 +1,12 @@
 let myString: string = "Hello World";
 let myNumber: number = 1;
 let myBoolean: boolean = true;
-let Any: any = `Anything @ ${123}`; // can be anithing string number or boolean // 
+let Any: any = `Anything @ ${123}`; // can be anithing string number or boolean //
+
+let stringArr: string[] = ['hello' , 'world'];
+let numberArr: number[] = [1 , 2 , 3];
+let booleanArr: boolean[] = [true , false];
+let anyArr: any[] = ['hello' , 1 , true];
 
 console.log(myString , myNumber , myBoolean , Any);
+console.log(stringArr , numberArr , booleanArr , anyArr);
